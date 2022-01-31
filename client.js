1bfdae878526cd6f298a873ef615623bd2029e1e
Dwight Lyle <dwightjl@gmail.com>
2022-01-31 14:24:34 -0800

    Add new example code for Chainlink Solana OCR2 data feeds
    Co-Authored-By: pappas999 <harry@genesisblockchain.com.au>
    Co-Authored-By: Blaž Hrastnik <blaz@mxxn.io>

---
+++ client.js
@@ -0,0 +1,61 @@
+// Parse arguments
+// --program - [Required] The account address for your deployed program.
+// --feed - The account address for the Chainlink data feed to retrieve
+const args = require('minimist')(process.argv.slice(2));
+
+// Initialize Anchor and provider
+const anchor = require("@project-serum/anchor");
+const provider = anchor.Provider.env();
+// Configure the cluster.
+anchor.setProvider(provider);
+
+const CHAINLINK_PROGRAM_ID = "DWqYEinRbZWtuq1DiDYvmexAKFoyjSyazZZUvdgPHT5g";
+
+// Data feed account address
+// Default is SOL / USD
+const default_feed = "7ndYj66ec3yPS58kRpodch3n8TEkCiaiy8tZ8Szb3BjP";
+const CHAINLINK_FEED = args['feed'] || default_feed;
+
+async function main() {
+  // Read the generated IDL.
+  const idl = JSON.parse(
+    require("fs").readFileSync("./target/idl/chainlink_solana_demo.json", "utf8")
+  );
+
+  // Address of the deployed program.
+  const programId = new anchor.web3.PublicKey(args['program']);
+
+  // Generate the program client from IDL.
+  const program = new anchor.Program(idl, programId);
+
+  //create an account to store the price data
+  const priceFeedAccount = anchor.web3.Keypair.generate();
+
+  console.log('priceFeedAccount public key: ' + priceFeedAccount.publicKey);
+  console.log('user public key: ' + provider.wallet.publicKey);
+
+  // Execute the RPC.
+  let tx = await program.rpc.execute({
+    accounts: {
+      decimal: priceFeedAccount.publicKey,
+      user: provider.wallet.publicKey,
+      chainlinkFeed: CHAINLINK_FEED,
+      chainlinkProgram: CHAINLINK_PROGRAM_ID,
+      systemProgram: anchor.web3.SystemProgram.programId
+    },
+    options: { commitment: "confirmed" },
+    signers: [priceFeedAccount],
+  });
+
+  console.log("Fetching transaction logs...");
+  let t = await provider.connection.getConfirmedTransaction(tx, "confirmed");
+  console.log(t.meta.logMessages);
+  // #endregion main
+
+  // Fetch the account details of the account containing the price data
+  const latestPrice = await program.account.decimal.fetch(priceFeedAccount.publicKey);
+  console.log('Price Is: ' + latestPrice.value)
+}
+
+console.log("Running client...");
+main().then(() => console.log("Success"));
---
+++ tests/chainlink-solana-demo-int-test.ts
@@ -0,0 +1,54 @@
+import * as anchor from '@project-serum/anchor';
+import * as fs from 'fs';
+import { Program, BN } from '@project-serum/anchor';
+import { ChainlinkSolanaDemo } from '../target/types/chainlink_solana_demo';
+const assert = require("assert");
+
+const CHAINLINK_PROGRAM_ID = "DWqYEinRbZWtuq1DiDYvmexAKFoyjSyazZZUvdgPHT5g";
+// SOL/USD feed account
+const CHAINLINK_FEED = "7ndYj66ec3yPS58kRpodch3n8TEkCiaiy8tZ8Szb3BjP";
+
+describe('chainlink-solana-demo', () => {
+  const provider = anchor.Provider.env();
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(provider);
+
+  it('Query SOL/USD Price Feed!', async () => {
+
+    // Get the generated IDL
+    const idl = JSON.parse(
+      require("fs").readFileSync("./target/idl/chainlink_solana_demo.json", "utf8")
+    );
+
+    // Address of the deployed program.
+    const programId = new anchor.web3.PublicKey("JC16qi56dgcLoaTVe4BvnCoDL6FhH5NtahA7jmWZFdqm");
+
+    // Generate the program client from IDL and program Id.
+    const program = new anchor.Program(idl, programId);
+
+    //create an account to store the price data
+    const priceFeedAccount = anchor.web3.Keypair.generate();
+
+    // Execute the RPC.
+    let tx = await program.rpc.execute({
+      accounts: {
+        decimal: priceFeedAccount.publicKey,
+        user: provider.wallet.publicKey,
+        chainlinkFeed: CHAINLINK_FEED,
+        chainlinkProgram: CHAINLINK_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId
+      },
+      options: { commitment: "confirmed" },
+      signers: [priceFeedAccount],
+    });
+
+    // Fetch the account details of the account containing the price data
+    const latestPrice = await program.account.decimal.fetch(priceFeedAccount.publicKey);
+    console.log('Price Is: ' + latestPrice.value)
+
+    // Ensure the price returned is a positive value
+    assert.ok(latestPrice.value > 0);
+
+  });
+});

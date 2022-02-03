3792e5615dde6015475947db7f14cbcacfd5799e
pappas999 <harry@genesisblockchain.com.au>
2022-02-03 20:58:32 +1030

    updates

--- read-data.js
+++ read-data.js
@@ -6,6 +6,9 @@ const anchor = require("@project-serum/anchor");
 const web3 = require("@solana/web3.js");
 const borsh = require("borsh")
 const provider = anchor.Provider.env();
+const EventParser = require("@project-serum/anchor");
+const Coder = require("@project-serum/anchor");
+
 anchor.setProvider(provider);
 
 const CHAINLINK_PROGRAM_ID = "CaH12fwNTKJAG8PxEvo9R96Zc2j8qNHZaFj8ZW49yZNT";
@@ -29,6 +32,13 @@ class Assignable {
 class Round extends Assignable {
 }
 
+// Transmission class, used for getting latest price data without making a tx
+class Transmissions extends Assignable {
+}
+
+class Transmission extends Assignable {
+}
+
 const Scope = {
   Version: { version: {} },
   Decimals: { decimals: {} },
@@ -45,53 +55,133 @@ const roundSchema = new Map([[Round, {
   ]
 }]]);
 
+const transmissionsSchema = new Map([[Transmissions, {
+  kind: 'struct', fields: [
+    ['versions', 'u8'],
+    ['store', [32]],
+    ['writer', [32]],
+    ['description', [32]],
+    ['decimals', 'u32'],
+    ['flagging_threshold', 'u32'],
+    ['latest_round_id', 'u32'],
+    ['granularity', 'u8'],
+    ['live_length', 'u32'],
+    ['live_cursor', 'u32'],
+    ['historical_cursor', 'u32']
+  ]
+}]]);
+
+const transmissionSchema = new Map([[Transmission, {
+  kind: 'struct', fields: [
+    ['timestamp', 'u64'],
+    ['answer', 'u128']
+  ]
+}]]);
+
 async function main() {
 
   //First we create a connection to the store program for Chainlink feeds on devnet
   let storeIdl = JSON.parse(require("fs").readFileSync('./store.json'));
   const storeProgram = new anchor.Program(storeIdl, CHAINLINK_PROGRAM_ID, provider);
-
-
-  //Call the query function on the store program, passing in the price feed account address we want to pull data from
-  let tx = await storeProgram.rpc.query(
-    Scope.LatestRoundData,
-    {
-      accounts: { feed: CHAINLINK_FEED },
-      options: { commitment: "confirmed" },
-    }
-  );
-
-  // Wait to get the data
-  let t = await provider.connection.getConfirmedTransaction(tx, "confirmed");
-  //console.log(t.meta.logMessages);
-
-  const prefix = "Program return: ";
-  let log = t.meta.logMessages.find((log) => log.startsWith(prefix));
-  log = log.slice(prefix.length);
-  let [_key, data] = log.split(" ", 2);
-  let buf = Buffer.from(data, "base64");
-  //console.log(JSON.stringify(buf));
-
-
-  let result = borsh.deserialize(roundSchema, Round, buf)['answer']
-  let resultFormatted = (byteArrayToLong(result) / DIVISOR)
-  console.log(resultFormatted)
-
-
-
-  console.log('---------------------')
-  console.log('Now trying with getAccountInfo')
+/*
+  console.log('trying with getAccountInfo')
   const publicKey = new web3.PublicKey(
     CHAINLINK_FEED
   );
-  let tmp = await provider.connection.getAccountInfo(publicKey,{"encoding": "jsonParsed"})
-  console.log(tmp['data'])
-  let result2 = borsh.deserializeUnchecked(roundSchema, Round, tmp['data'])
-  console.log(result2)
-  let resultFormatted2 = (byteArrayToLong(result2.answer) / DIVISOR)
-    console.log('timestamp: ' + result2.timestamp)
-  console.log(resultFormatted2)
 
+  // The feed account is essentially a 128 byte header we can automatically parse with Anchor,
+  // then we need to calculate the transmission offset and read the round there via borsh.
+
+  // get the transmission header
+  const headerStart = 8
+
+  const accountData = await provider.connection._rpcRequest('getAccountInfo',
+    [
+      CHAINLINK_FEED,
+      {
+        encoding: 'base64',
+        commitment: 'finalized',
+        dataSlice: { "offset": headerStart, "length": 128 }
+      }
+    ]
+  );
+  //note, no error handling for acct not found - blindly going after data
+  //console.log(accountData)
+
+  const buf = Buffer.from(accountData.result.value.data[0], 'base64');
+  //console.log(buf)
+
+  // deserialize the buffer into a header
+  let resultHeader = borsh.deserializeUnchecked(transmissionsSchema, Transmissions, buf)
+  //console.log(resultHeader)
+
+  console.log('Feed Desc: ' + String.fromCharCode.apply(String, resultHeader.description))
+
+
+  // set transmission
+  let cursor = resultHeader.live_cursor
+  let liveLength = resultHeader.live_length
+  //console.log('live cursor: ' + cursor)
+  //console.log('live liveLength: ' + liveLength)
+  if (cursor == 0) {
+    cursor = liveLength
+  }
+  cursor = cursor - 1
+
+  const transmissionLength = 24
+  //let transmissionOffset = 8 + 128 + (cursor * transmissionLength) //13215856 // THIS PART ISN'T WORKING
+  let transmissionOffset = 136 + (24 * 1023)
+  console.log('transmission offset: ' + transmissionOffset)
+
+  console.log('getting transmission data')
+
+  // do account info again with new paramrs to get the transmission
+  const transmissionData = await provider.connection._rpcRequest('getAccountInfo',
+    [
+      CHAINLINK_FEED,
+      {
+        encoding: 'base64',
+        commitment: 'confirmed',
+        dataSlice: { "offset": transmissionOffset, "length": transmissionLength }
+      }
+    ]
+  );
+
+  //console.log(transmissionData)
+  //console.log('buffer length: ' + Buffer.byteLength(transmissionData.result.value.data[0], 'base64'))
+  //  console.log(JSON.stringify(transmissionData))
+
+
+  const transmissionDataBuf = Buffer.from(transmissionData.result.value.data[0], 'base64');
+  //console.log(transmissionDataBuf)
+
+  //parse v1 transmission and return answer
+  //deserialize tranmission into object
+  let resultTransmission = borsh.deserializeUnchecked(transmissionSchema, Transmission, transmissionDataBuf)
+  console.log(JSON.stringify(resultTransmission))
+
+  // parse transmission
+  //unix time 1643763291
+
+  console.log('timestamp  is: ' + resultTransmission.timestamp)
+  console.log('latest price is: ' + resultTransmission.answer)
+
+*/
+  console.log('------------------------------------------------------')
+  console.log('now trying with streams/events')
+
+
+
+
+//  const idl = {...}; // your idl here
+  const coder = new anchor.Coder(storeIdl);
+  const parser = new anchor.EventParser('EdWr4ww1Dq82vPe8GFjjcVPo2Qno3Nhn6baCgM3dCy28', coder);
+
+  // Parse a string[] of logs from a transaction.
+  parser.parseLogs('logs', (event) => console.log(event));
+
+  // Parse a single log.
+  coder.events.decode(log);
 
 }
 

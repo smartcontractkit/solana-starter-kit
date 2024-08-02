f275efcaa6e42dc1a9f1fd22914c01b0a269e3b2
qingyangkong <frank.kong@smartcontract.com>
2024-08-02 17:23:00 +0800

    update anchor-lang to 0.30.1

--- client.js
+++ client.js
@@ -1,57 +1,34 @@
-// Parse arguments
-// --program - [Required] The account address for your deployed program.
-// --feed - The account address for the Chainlink data feed to retrieve
-const args = require('minimist')(process.argv.slice(2));
-
 // Initialize Anchor and provider
-const anchor = require("@project-serum/anchor");
-const provider = anchor.AnchorProvider.env();
-// Configure the cluster.
-anchor.setProvider(provider);
+const anchor = require("@coral-xyz/anchor")
+anchor.setProvider(anchor.AnchorProvider.env());
 
 const CHAINLINK_PROGRAM_ID = "HEvSKofvBgfaexv23kMabbYqxasxU3mQ4ibBMEmJWHny";
 const DIVISOR = 100000000;
 
 // Data feed account address
-// Default is SOL / USD
-const default_feed = "99B2bTijsU6f1GCT73HmdR7HCFFjGMBcPZY6jZ96ynrR";
-const CHAINLINK_FEED = args['feed'] || default_feed;
+// Default is ETH / USD
+const CHAINLINK_FEED = "669U43LNHx7LsVj95uYksnhXUfWKDsdzVqev3V4Jpw3P"
 
 async function main() {
-  // Read the generated IDL.
-  const idl = JSON.parse(
-    require("fs").readFileSync("./target/idl/chainlink_solana_demo.json", "utf8")
-  );
-
-  // Address of the deployed program.
-  const programId = new anchor.web3.PublicKey(args['program']);
-
-  // Generate the program client from IDL.
-  const program = new anchor.Program(idl, programId);
+  // Create program client
+  const program = anchor.workspace.ChainlinkSolanaDemo
 
   //create an account to store the price data
   const priceFeedAccount = anchor.web3.Keypair.generate();
-
   console.log('priceFeedAccount public key: ' + priceFeedAccount.publicKey);
-  console.log('user public key: ' + provider.wallet.publicKey);
 
   // Execute the RPC.
-  let tx = await program.rpc.execute({
-    accounts: {
+  let transactionSignature = await program.methods
+    .execute()
+    .accounts({
       decimal: priceFeedAccount.publicKey,
-      user: provider.wallet.publicKey,
       chainlinkFeed: CHAINLINK_FEED,
       chainlinkProgram: CHAINLINK_PROGRAM_ID,
-      systemProgram: anchor.web3.SystemProgram.programId
-    },
-    options: { commitment: "confirmed" },
-    signers: [priceFeedAccount],
-  });
-
-  console.log("Fetching transaction logs...");
-  let t = await provider.connection.getConfirmedTransaction(tx, "confirmed");
-  console.log(t.meta.logMessages);
-  // #endregion main
+    })
+    .signers([priceFeedAccount])
+    .rpc()
+  
+  console.log(`Transaction Signature: ${transactionSignature}`)
 
   // Fetch the account details of the account containing the price data
   const latestPrice = await program.account.decimal.fetch(priceFeedAccount.publicKey);
--- programs/chainlink_solana_demo/src/lib.rs
+++ programs/chainlink_solana_demo/src/lib.rs
@@ -2,7 +2,59 @@ use anchor_lang::prelude::*;
 
 use chainlink_solana as chainlink;
 
-declare_id!("JC16qi56dgcLoaTVe4BvnCoDL6FhH5NtahA7jmWZFdqm");
+declare_id!("GEgDWT7Cc8H5S1o2YnTp3umiciazQj5fKbftPXkc2TsL");
+
+#[program]
+pub mod chainlink_solana_demo {
+
+    use chainlink_solana::Round;
+
+    use super::*;
+    pub fn execute(
+        ctx: Context<Execute>
+    ) -> Result<()> {
+        let round: Round = chainlink::latest_round_data(
+            ctx.accounts.chainlink_program.to_account_info(),
+            ctx.accounts.chainlink_feed.to_account_info(),
+        )?;
+
+        let description: String = chainlink::description(
+            ctx.accounts.chainlink_program.to_account_info(),
+            ctx.accounts.chainlink_feed.to_account_info(),
+        )?;
+
+        let decimals: u8 = chainlink::decimals(
+            ctx.accounts.chainlink_program.to_account_info(), 
+            ctx.accounts.chainlink_feed.to_account_info())?;
+
+        let decimal: &mut Account<Decimal> = &mut ctx.accounts.decimal;
+        decimal.value = round.answer;
+        decimal.decimals = u32::from(decimals);
+
+        let decimal_print: Decimal = Decimal::new(round.answer, u32::from(decimals));
+        msg!("{} price is {}", description, decimal_print);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Execute<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = 100,
+    )]
+    pub decimal: Account<'info, Decimal>,
+
+    /// CHECK: We're reading data from this specified chainlink feed
+    pub chainlink_feed: AccountInfo<'info>,
+    /// CHECK: This is the Chainlink program library on Devnet
+    pub chainlink_program: AccountInfo<'info>,
+    /// CHECK: This is the devnet system program
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct Decimal {
@@ -12,7 +64,7 @@ pub struct Decimal {
 
 impl Decimal {
     pub fn new(value: i128, decimals: u32) -> Self {
-        Decimal { value, decimals }
+        Decimal {value, decimals}
     }
 }
 
@@ -22,57 +74,12 @@ impl std::fmt::Display for Decimal {
         if scaled_val.len() <= self.decimals as usize {
             scaled_val.insert_str(
                 0,
-                &vec!["0"; self.decimals as usize - scaled_val.len()].join(""),
+                &vec!["0"; self.decimals as usize - scaled_val.len()].join("")
             );
-            scaled_val.insert_str(0, "0.");
+            scaled_val.insert_str(0, "0.")
         } else {
             scaled_val.insert(scaled_val.len() - self.decimals as usize, '.');
         }
         f.write_str(&scaled_val)
     }
-}
-
-#[program]
-pub mod chainlink_solana_demo {
-    use super::*;
-        pub fn execute(ctx: Context<Execute>) -> ProgramResult  {
-        let round = chainlink::latest_round_data(
-            ctx.accounts.chainlink_program.to_account_info(),
-            ctx.accounts.chainlink_feed.to_account_info(),
-        )?;
-
-        let description = chainlink::description(
-            ctx.accounts.chainlink_program.to_account_info(),
-            ctx.accounts.chainlink_feed.to_account_info(),
-        )?;
-
-        let decimals = chainlink::decimals(
-            ctx.accounts.chainlink_program.to_account_info(),
-            ctx.accounts.chainlink_feed.to_account_info(),
-        )?;
-
-        // Set the account value
-        let decimal: &mut Account<Decimal> = &mut ctx.accounts.decimal;
-        decimal.value=round.answer;
-        decimal.decimals=u32::from(decimals);
-
-        // Also print the value to the program output
-        let decimal_print = Decimal::new(round.answer, u32::from(decimals));
-        msg!("{} price is {}", description, decimal_print);
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Execute<'info> {
-    #[account(init, payer = user, space = 100)]
-    pub decimal: Account<'info, Decimal>,
-    #[account(mut)]
-    pub user: Signer<'info>,
-    /// CHECK: We're reading data from this specified chainlink feed
-    pub chainlink_feed: AccountInfo<'info>,
-    /// CHECK: This is the Chainlink program library on Devnet
-    pub chainlink_program: AccountInfo<'info>,
-    /// CHECK: This is the devnet system program
-    pub system_program: Program<'info, System>,
 }
